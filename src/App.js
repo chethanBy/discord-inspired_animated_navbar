6fc4b8b3034bea595f44784806138825a2d7c646
AhriMain <chethanawe123@gmail.com>
2022-09-16 16:34:22 +0530

    added message contaner and nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Sidebar from "./Sidebar";
+import ChannelBar from "./components/ChannelBar";
+import ContentContainer from "./components/ContentContainer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
+      <ChannelBar />
+      <ContentContainer />
     </div>
   );
 }
